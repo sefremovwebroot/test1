e3b4f41bcc15e43ddb9cd54f4eab3dd4345e3d2d
Sergey Efremov <9492393781p@gmail.com>
2019-10-22 19:48:13 -0700

    more code

--- sample1.py
+++ sample1.py
@@ -1,3 +1,31 @@
+# https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/python
+# Remove String Spaces
+def no_space(x):
+    return x.replace(" ", "")
+
+###
+# https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/python
+# Remove String Spaces
+def no_space(x):
+    return "".join(x.split())
+
+###
+# https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/python
+# Remove String Spaces
+no_space = lambda s: ''.join(filter(lambda ch: not ch == ' ', s))
+
+###
+# https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/python
+# Grasshopper - Terminal game move function
+def move(position, roll):
+    return position + roll * 2
+
+###
+# https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/python
+# Grasshopper - Terminal game move function
+move = lambda p, r : p + r * 2
+
+
 # comment1
 # comment2
 # comment3
